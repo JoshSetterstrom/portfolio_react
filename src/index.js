481780fb8e0cdcb3958a6f6088bfea35353d4c3c
unknown <joshsetterstrom@gmail.com>
2024-12-04 04:08:01 -0800

    20241204

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './home';
 import Projects from './projects';
+import asd from './assets/asd.png';
+import gradient2 from './assets/gradient2.png'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,8 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <main>
+            <img src={gradient2} style={{left: "-2%", width: 2800, display: 'flex', position: 'absolute', zIndex: 2}}/>
+            <img src={asd} style={{height: "100vh", display: 'flex', position: 'absolute', zIndex: 1}}/>
             {/* <Home/> */}
-            <Projects />
+            {/* <Projects /> */}
         </main>
     </React.StrictMode>
 );
